3fca3e5bac8861b849335806e4a1ad77510a3b23
qiansheng91 <xin.zhang@tutamail.com>
2024-06-06 08:32:05 +0800

    add go nvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,5 +34,22 @@ return {
        "html", "css", "c", "cpp"
   		},
   	},
+  },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   }
+
+
 }
